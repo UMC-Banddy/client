1dcf889bd512252c99032375137a59a32dd83231
hywznn <choihj0510@naver.com>
2025-07-04 12:57:55 +0900

    모바일용 메타태그 적용

--- src/widgets/Layout/Layout.tsx
+++ src/widgets/Layout/Layout.tsx
@@ -3,11 +3,11 @@ import BottomBar from "@/widgets/Layout/BottomBar";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
+    <div className="relative min-h-screen flex flex-col bg-gray-50">
       <header className="p-4 bg-black text-white text-xl font-bold">
         Banddy
       </header>
-      <main className="flex-1 flex flex-col items-center justify-center">
+      <main className="flex-1 flex flex-col items-center justify-center pb-16">
         <Outlet />
       </main>
       <footer className="p-2 bg-black text-white text-center text-xs">
