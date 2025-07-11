7547e43d7bdf27ea27db248962a494b2943e1e0d
hywznn <choihj0510@naver.com>
2025-07-11 21:12:08 +0900

    fix : no-sound svg 배경 삭제

--- src/pages/My/_components/Archive/ArchiveItem.tsx
+++ src/pages/My/_components/Archive/ArchiveItem.tsx
@@ -1,4 +1,4 @@
-import no_sound from "@/assets/icons/no-sound.svg";
+import no_sound from "@/assets/icons/home/no-sound.svg";
 
 interface ArchiveItemProps {
   coverUrl?: string;
@@ -6,22 +6,36 @@ interface ArchiveItemProps {
   muted?: boolean;
 }
 
-export default function ArchiveItem({ coverUrl, title, muted }: ArchiveItemProps) {
+export default function ArchiveItem({
+  coverUrl,
+  title,
+  muted,
+}: ArchiveItemProps) {
   return (
     <div className="flex flex-col items-start">
       <div className="relative w-[29vw] h-[29vw] overflow-hidden flex items-center justify-center">
         {coverUrl ? (
-          <img src={coverUrl} alt={title} className="w-full h-full object-cover" />
+          <img
+            src={coverUrl}
+            alt={title}
+            className="w-full h-full object-cover"
+          />
         ) : (
           <span className="text-[#FFFFFF] text-3xl" />
         )}
         {muted && (
-          <div className="absolute bottom-[1vw] right-[1vw] w-[8vw] h-[8vw] rounded-full bg-black/70 flex items-center justify-center shadow-lg">
-            <img src={no_sound} alt="no-sound" className="w-full h-full object-cover" />
+          <div className="absolute bottom-[1vw] right-[1vw] w-[8vw] h-[8vw] rounded-full  flex items-center justify-center shadow-lg">
+            <img
+              src={no_sound}
+              alt="no-sound"
+              className="w-full h-full object-cover"
+            />
           </div>
         )}
       </div>
-      <span className="text-hakgyo-r-14 text-[#FFFFFF] text-left mt-[1vh] truncate w-full">{title}</span>
+      <span className="text-hakgyo-r-14 text-[#FFFFFF] text-left mt-[1vh] truncate w-full">
+        {title}
+      </span>
     </div>
   );
 }
