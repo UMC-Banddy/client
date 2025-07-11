2eaccc7e81932d5da71cb825f0ce93e9216b3a07
Junhyeok <cjh64866@naver.com>
2025-07-11 15:38:01 +0900

    feat: MuiDialog component 생성

---
+++ src/shared/components/MuiDialog.tsx
@@ -0,0 +1,25 @@
+import { Dialog } from "@mui/material";
+
+interface ModalProps {
+  children: React.ReactNode;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+
+const Modal = ({ children, open, setOpen }: ModalProps) => {
+  return (
+    <Dialog
+      onClose={() => setOpen(false)}
+      open={open}
+      sx={{
+        "& .MuiDialog-paper": {
+          borderRadius: "14px",
+        },
+      }}
+    >
+      {children}
+    </Dialog>
+  );
+};
+
+export default Modal;
