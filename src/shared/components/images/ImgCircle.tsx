33fe7622fccd81e3c05bc6c8b5cf4ede6bc394b4
hywznn <choihj0510@naver.com>
2025-07-17 16:38:22 +0900

    feat : 아이콘 컴포넌트 색깔 추가 및 생성

--- src/shared/components/images/ImgCircle.tsx
+++ src/shared/components/images/ImgCircle.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from "react";
 
 interface ImgCircleProps {
   size?: number;
-  color: "red" | "gray";
+  color: "red" | "gray" | "red-400" | "gray-700" | "gray-200";
 }
 
 const ImgCircle = ({
@@ -11,13 +11,20 @@ const ImgCircle = ({
   color,
   ...props
 }: PropsWithChildren<ImgCircleProps>) => {
+  // 색상 매핑
+  const bgColor =
+    color === "red" || color === "red-400"
+      ? "#B42127"
+      : color === "gray" || color === "gray-700"
+      ? "#292929"
+      : color === "gray-200"
+      ? "#CACACA"
+      : undefined;
+
   return (
     <div
-      className={clsx("flex items-center justify-center rounded-full", {
-        "bg-[#CACACA]": color === "gray",
-        "bg-[#B42127]": color === "red",
-      })}
-      style={{ width: size, height: size }}
+      className={clsx("flex items-center justify-center rounded-full")}
+      style={{ width: size, height: size, backgroundColor: bgColor }}
       {...props}
     ></div>
   );
--- src/shared/components/images/types/imgSrc.ts
+++ src/shared/components/images/types/imgSrc.ts
@@ -1,4 +1,10 @@
 export interface ImgSrcProps {
   size?: number;
-  color: "red" | "gray";
+  color: "red" | "gray" | "red-400" | "gray-700" | "gray-200";
+}
+
+// 색깔이 변경되어서 그 작업 하고 돌아오기
+export interface ImgLinksProps {
+  size?: number;
+  color: "red-400" | "gray-700";
 }
