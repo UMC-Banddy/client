a91886e843feeda56d0a63af03ecb6edaecaa610
hywznn <choihj0510@naver.com>
2025-07-17 16:38:50 +0900

    feat : 모집중 뱃지 추가 및 변경

---
+++ src/pages/Home/_components/people/RecruitBadge.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const RecruitBadge = () => (
+  <div className="absolute -top-2 left-1 bg-red-400 text-white text-xs font-bold rounded-xl px-2 py-0.5 shadow z-10">
+    모집중!
+  </div>
+);
+
+export default RecruitBadge;
