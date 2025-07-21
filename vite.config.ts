0c32c87d7b3381bde93ea941fdca24cd48fd15a5
hywznn <choihj0510@naver.com>
2025-07-21 17:06:54 +0900

    chore : svgr 설치

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -58,6 +59,7 @@ export default defineConfig({
         ],
       },
     }),
+    svgr(),
   ],
   resolve: {
     alias: {
