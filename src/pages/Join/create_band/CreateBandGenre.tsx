c96548666063c1b5d29cb157b6dadc7343809499
Junhyeok <cjh64866@naver.com>
2025-07-30 14:10:29 +0900

    design: JoinHeader에 맞게 padding 및 레이아웃 수정

--- src/pages/Join/create_band/CreateBandGenre.tsx
+++ src/pages/Join/create_band/CreateBandGenre.tsx
@@ -12,47 +12,49 @@ const CreateBandGenre = () => {
 
   const navigate = useNavigate();
   return (
-    <main className="relative min-h-screen w-[393px] mx-auto px-[24px] pt-[16px] pb-[200px]">
+    <main className="relative min-h-screen w-[393px] mx-auto px-[16px] pt-[16px] pb-[200px]">
       <JoinHeader
         enableConfirmBtn={toggledGenre.length > 0}
         onClick={() => navigate("/join/create-band")}
       />
 
-      <section className="flex flex-col gap-[28px] mt-[24px]">
-        <p className="text-hakgyo-b-24 text-[#E9E9E9]">추구하는 장르</p>
-        {toggledGenre.length > 0 && (
-          <div className="flex flex-nowrap gap-[12px] overflow-x-auto">
-            {toggledGenre.map((genre) => (
-              <GenreStatusBtn
-                key={genre}
-                onClick={() =>
-                  setToggledGenre(toggledGenre.filter((id) => id !== genre))
-                }
-              >
-                {genres[genre].content}
-              </GenreStatusBtn>
-            ))}
-          </div>
-        )}
-        <div className="h-[0.5px] bg-[#E9E9E9]"></div>
-      </section>
+      <section className="px-[8px]">
+        <section className="flex flex-col gap-[28px] mt-[24px]">
+          <p className="text-hakgyo-b-24 text-[#E9E9E9]">추구하는 장르</p>
+          {toggledGenre.length > 0 && (
+            <div className="flex flex-nowrap gap-[12px] overflow-x-auto">
+              {toggledGenre.map((genre) => (
+                <GenreStatusBtn
+                  key={genre}
+                  onClick={() =>
+                    setToggledGenre(toggledGenre.filter((id) => id !== genre))
+                  }
+                >
+                  {genres[genre].content}
+                </GenreStatusBtn>
+              ))}
+            </div>
+          )}
+          <div className="h-[0.5px] bg-[#E9E9E9]"></div>
+        </section>
 
-      <section className="flex flex-wrap justify-center gap-[16px] mt-[44px] mx-[-12px]">
-        {genres.map((genre) => (
-          <GenreToggleBtn
-            key={genre.id}
-            onClick={() => {
-              if (toggledGenre.includes(genre.id)) {
-                setToggledGenre(toggledGenre.filter((id) => id !== genre.id));
-              } else {
-                setToggledGenre([...toggledGenre, genre.id]);
-              }
-            }}
-            toggled={toggledGenre.includes(genre.id)}
-          >
-            {genre.content}
-          </GenreToggleBtn>
-        ))}
+        <section className="flex flex-wrap justify-center gap-[16px] mt-[44px] mx-[-12px]">
+          {genres.map((genre) => (
+            <GenreToggleBtn
+              key={genre.id}
+              onClick={() => {
+                if (toggledGenre.includes(genre.id)) {
+                  setToggledGenre(toggledGenre.filter((id) => id !== genre.id));
+                } else {
+                  setToggledGenre([...toggledGenre, genre.id]);
+                }
+              }}
+              toggled={toggledGenre.includes(genre.id)}
+            >
+              {genre.content}
+            </GenreToggleBtn>
+          ))}
+        </section>
       </section>
     </main>
   );
