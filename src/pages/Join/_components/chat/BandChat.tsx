9c0e4db2b4ec13969eebc7614bcfd758ef390c37
Junhyeok <cjh64866@naver.com>
2025-08-12 16:51:30 +0900

    feat: Join 페이지 API 연결

--- src/pages/Join/_components/chat/BandChat.tsx
+++ src/pages/Join/_components/chat/BandChat.tsx
@@ -2,31 +2,48 @@ import host from "@/assets/icons/join/ic_host.svg";
 import { useNavigate } from "react-router-dom";
 
 interface BandChatProps {
+  id: number;
+  name: string;
+  thumbnail: string | null;
+  unreadCount: number | null;
   isHost?: boolean;
 }
 
-const BandChat = ({ isHost = false }: BandChatProps) => {
+const BandChat = ({
+  id,
+  name,
+  thumbnail,
+  unreadCount,
+  isHost = false,
+}: BandChatProps) => {
   const navigate = useNavigate();
 
   return (
     <button
       className="flex justify-between items-center w-full bg-transparent border-none cursor-pointer"
       onClick={() => {
-        navigate("band-recruit");
+        if (isHost) {
+          navigate(`/join/band-recruit/${id}`);
+        }
       }}
     >
       <div className="flex items-center gap-[12px]">
-        <div className="bg-[#777] size-[50px]"></div>
+        <div
+          className="size-[50px] bg-[#777] bg-cover bg-center"
+          style={{ backgroundImage: `url(${thumbnail})` }}
+        ></div>
         <div className="flex flex-col gap-[4px] text-start">
           <p className="flex items-center gap-[4px] text-hakgyo-r-16 text-[#fff]">
             {isHost && <img className="size-[18px]" src={host} alt="" />}
-            저희왜색짙어요
+            {name}
           </p>
         </div>
       </div>
-      <div className="flex justify-center items-center size-[22px] rounded-full bg-[#C7242D] text-[#fff] text-wanted-sb-10">
-        11
-      </div>
+      {unreadCount && (
+        <div className="flex justify-center items-center size-[22px] rounded-full bg-[#C7242D] text-[#fff] text-wanted-sb-10">
+          {unreadCount}
+        </div>
+      )}
     </button>
   );
 };
