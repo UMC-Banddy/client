0cd5b3c9898c7ff356310c30efd6adba38e79460
Junhyeok <cjh64866@naver.com>
2025-08-20 17:46:13 +0900

    feat: BandRecruit 세션 이미지 매핑 추가

---
+++ src/assets/icons/join/band_recruit/index.ts
@@ -0,0 +1,8 @@
+export { default as mic } from "./ic_mic_tiny.svg";
+export { default as electricGuitar } from "./ic_guitar_tiny.svg";
+export { default as acousticGuitar } from "./ic_acoustic_guitar_tiny.svg";
+export { default as bass } from "./ic_bass_tiny.svg";
+export { default as drum } from "./ic_drum_tiny.svg";
+export { default as piano } from "./ic_piano_tiny.svg";
+export { default as trumpet } from "./ic_trumpet_tiny.svg";
+export { default as violin } from "./ic_violin_tiny.png";
